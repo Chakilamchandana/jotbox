3d9e66df0fab0304cce2f7141a66b0c98e5181f9
chandana <srichandanach09@gmail.com>
2023-09-04 20:43:29 -0400

    modified App.jsx and ran build

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Footer from './Footer';
 import Note from './Note';
-import jots from '../jots';
 import CreateNotes from "./CreateNotes";
 
 function App(){
